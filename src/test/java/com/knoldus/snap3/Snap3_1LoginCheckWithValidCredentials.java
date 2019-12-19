09c43ecacda5e32e65a8b30722815fc913da99de
komal1824 <komalrajpal24@gmail.com>
2019-12-19 12:31:15 +0530

    Login scuccessful test case added for snap3

--- src/test/java/com/knoldus/snap3/Snap3_1LoginCheckWithValidCredentials.java
+++ src/test/java/com/knoldus/snap3/Snap3_1LoginCheckWithValidCredentials.java
@@ -1,5 +1,6 @@
 package com.knoldus.snap3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -18,7 +19,24 @@ public class Snap3_1LoginCheckWithValidCredentials {
 		driver.manage().window().maximize();
 		String url = driver.getCurrentUrl();
 		System.out.println(url);
+		//login form presence 
+		driver.findElement(By.xpath("//form[@class='login-form']")).isDisplayed();
 		
+		driver.findElement(By.xpath("//form[@class='login-form']//input[@name='email']")).isDisplayed();
+		driver.findElement(By.xpath("//form[@class='login-form']//input[@name='email']")).sendKeys("fandango@fandango.com");
+		
+		driver.findElement(By.xpath("//form[@class='login-form']//input[@name='password']")).isDisplayed();
+		driver.findElement(By.xpath("//form[@class='login-form']//input[@name='password']")).sendKeys("tqNxsV6z4a");
+		
+		driver.findElement(By.xpath("//button[contains(.,'Login ')]")).isDisplayed();
+		driver.findElement(By.xpath("//button[contains(.,'Login ')]")).click();
+		
+		url = driver.getCurrentUrl();
+		url.equals("http://snap3-staging.platform3.co/dashboard");
+		
+		System.out.println("Login successful");
+		
+		driver.close();
 		
 		
 	}
